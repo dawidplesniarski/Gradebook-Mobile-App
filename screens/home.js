13fdc8f4b90ff8804d2a06635d48460db6016ed4
dawidplesniarski <dawidplesniarski@gmail.com>
2020-06-23 21:39:03 +0200

    Navigation works

---
+++ screens/home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+
+export default class Home extends Component {
+    render() {
+        return(
+            <View style={{padding:150}}>
+                <Text>
+                    Home Screen
+                </Text>
+
+            </View>
+        );
+    }
+}
