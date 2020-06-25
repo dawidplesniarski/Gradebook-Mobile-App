8697122db93c8e9880159f81c96d40d150ca64f1
dawidplesniarski <dawidplesniarski@gmail.com>
2020-06-25 18:41:10 +0200

    Login Authentication works properly.

--- screens/home.js
+++ screens/home.js
@@ -19,43 +19,23 @@ export default function Home({ navigation }) {
             password: password
         }
 
-        // let res = await axios.post('https://node-app-4fun.herokuapp.com/users/login', params);
-        // console.log(res.name);
-        //
-        // console.log(res.data);
         await axios.post('https://node-app-4fun.herokuapp.com/users/login', params)
             .then(res => {
-                if(res.data._id===''){
-                    console.log('Wrong login or password')
+                if(res.data._id===undefined){
+                    setLogged(false);
                 }else{
                     setLogged(true);
                     setUserId(res.data._id);
                     setUserName(res.data.name);
                     setUserLastName(res.data.lastName);
                     setAlbumNo(res.data.albumNo);
+                    navigation.navigate('AnotherDetails');
                 }
             })
             .catch(err => {
                 console.log(err)
             });
     }
-
-    // useEffect(() => {
-    //     const params ={
-    //         login: login,
-    //         password: password
-    //     }
-    //     axios.post('https://node-app-4fun.herokuapp.com/users/login', params)
-    //         .then( res =>{
-    //             setUserId(res._id);
-    //             setUserName(res.name);
-    //             setUserLastName(res.lastName);
-    //             setAlbumNo(res.albumNo);
-    //         })
-    //         .catch(err =>{
-    //             console.log(err)
-    //         });
-    // },[]);
 
     return(
         <View navigation={navigation} style={styles.container}>
@@ -67,7 +47,7 @@ export default function Home({ navigation }) {
             </View>
             <Button text={'Register'} isButtonDark={true} onPress={()=> navigation.navigate('Details')}/>
             <Button text={'Details Screen'} isButtonDark={true} onPress={()=> navigation.navigate('AnotherDetails')}/>
-            <Text>id: {`${isUserLogged}`}</Text>
+            <Text>id: {`${userId}`}</Text>
         </View>
     )
 }
