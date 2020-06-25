097e8f5ba931e6d76d2457ea2f8088d3e11da2a0
dawidplesniarski <dawidplesniarski@gmail.com>
2020-06-25 18:32:42 +0200

    Login panel created. Login method works properly.

--- screens/anotherDetails.js
+++ screens/anotherDetails.js
@@ -1,5 +1,5 @@
 import React,{useEffect, useState} from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 import axios from 'axios';
 import set from '@babel/runtime/helpers/esm/set';
 
@@ -26,17 +26,53 @@ export default function({navigation}) {
     }
 
     return(
-        <View>
+        <View style={styles.container}>
             <FlatList
-                style={{borderWidth:1, height:'90%',width:'90%'}}
+                style={styles.flatList}
                 data={data}
+                keyExtractor={(item, index) => index.toString()}
                 renderItem={({item})=>(
-                    <View>
-                        <Text>{item.name}</Text>
+                    <View style={styles.flatListContainer}>
+                        <View style={styles.flatListView}>
+                            <Text style={styles.flatListText}>Imie: {item.name}</Text>
+                            <Text style={styles.flatListText}>Nazwisko: {item.lastName}</Text>
+                        </View>
+                        <View style={styles.flatListView}>
+                            <Text style={styles.flatListText}>Numer albumu: {item.name}</Text>
+                        </View>
+                        <View style={styles.flatListView}>
+                            <Text style={styles.flatListText}>Uczelnia: {item.university}</Text>
+                        </View>
                     </View>
                 )}
             />
         </View>
     );
-
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems: 'center'
+    },
+    flatList:{
+        width:'90%',
+        marginTop: 10,
+    },
+    flatListContainer:{
+        borderWidth:1,
+        borderRadius:7,
+        marginBottom: 20,
+    },
+    flatListView:{
+        flexDirection: 'row',
+        paddingTop:10,
+        paddingBottom:10,
+        marginLeft: 5,
+    },
+    flatListText:{
+        fontSize:14,
+        fontFamily: 'Futura',
+        paddingRight: 20,
+    }
+});
--- screens/home.js
+++ screens/home.js
@@ -1,13 +1,73 @@
-import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React, {Component, useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, TextInput} from 'react-native';
 import Button from '../components/Button';
+import axios from 'axios';
 
 export default function Home({ navigation }) {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [userId, setUserId] = useState(0);
+    const [userName, setUserName] = useState('');
+    const [userLastName, setUserLastName] = useState('');
+    const [userAlbumNo, setAlbumNo] = useState('');
+    const [isUserLogged, setLogged] = useState(false);
+
+    let loginUser = async () => {
+        const params = {
+            login: login,
+            password: password
+        }
+
+        // let res = await axios.post('https://node-app-4fun.herokuapp.com/users/login', params);
+        // console.log(res.name);
+        //
+        // console.log(res.data);
+        await axios.post('https://node-app-4fun.herokuapp.com/users/login', params)
+            .then(res => {
+                if(res.data._id===''){
+                    console.log('Wrong login or password')
+                }else{
+                    setLogged(true);
+                    setUserId(res.data._id);
+                    setUserName(res.data.name);
+                    setUserLastName(res.data.lastName);
+                    setAlbumNo(res.data.albumNo);
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    }
+
+    // useEffect(() => {
+    //     const params ={
+    //         login: login,
+    //         password: password
+    //     }
+    //     axios.post('https://node-app-4fun.herokuapp.com/users/login', params)
+    //         .then( res =>{
+    //             setUserId(res._id);
+    //             setUserName(res.name);
+    //             setUserLastName(res.lastName);
+    //             setAlbumNo(res.albumNo);
+    //         })
+    //         .catch(err =>{
+    //             console.log(err)
+    //         });
+    // },[]);
+
     return(
         <View navigation={navigation} style={styles.container}>
-            <Text style={{marginTop:30}}>New home screen</Text>
-            <Button text={'Details'} isButtonDark={true} onPress={()=> navigation.navigate('Details')}/>
-            <Button text={'Another Details Screen'} isButtonDark={true} onPress={()=> navigation.navigate('AnotherDetails')}/>
+            <Text style={styles.title}>Gradebook</Text>
+            <View style={styles.loginArea}>
+                <TextInput style={styles.textInput} placeholder={'Login'} onChangeText={text=> setLogin(text)}/>
+                <TextInput style={styles.textInput} placeholder={'Password'} secureTextEntry={true} onChangeText={text=>setPassword(text)}/>
+                <Button text={'Login'} isButtonDark={true} onPress={loginUser}/>
+            </View>
+            <Button text={'Register'} isButtonDark={true} onPress={()=> navigation.navigate('Details')}/>
+            <Button text={'Details Screen'} isButtonDark={true} onPress={()=> navigation.navigate('AnotherDetails')}/>
+            <Text>id: {`${isUserLogged}`}</Text>
         </View>
     )
 }
@@ -19,5 +79,30 @@ const styles = StyleSheet.create({
         justifyContent:'flex-start',
         alignItems: 'center',
         borderWidth:1
+    },
+    textInput:{
+        borderWidth: 1,
+        borderRadius:10,
+        backgroundColor: '#dadada',
+        width:'90%',
+        height: 35,
+        fontFamily: 'Futura',
+    },
+    loginArea:{
+        width:'90%',
+        borderWidth:1,
+        borderRadius: 12,
+        flexDirection : 'column',
+        justifyContent:'space-around',
+        alignItems: 'center',
+        height: '30%',
+        marginTop:30,
+        marginBottom:30,
+    },
+    title:{
+        fontSize: 30,
+        fontFamily: 'Futura',
+        textAlign: 'center',
+        marginTop:10,
     }
 })
