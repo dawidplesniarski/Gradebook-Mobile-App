39ed7a3fd73f41c7d557307412f1a66d16969189
dawidplesniarski <dawidplesniarski@gmail.com>
2020-06-25 17:07:00 +0200

    Fetch data from api to list works properly

--- screens/anotherDetails.js
+++ screens/anotherDetails.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import Button from '../components/Button';
-
+import React,{useEffect, useState} from 'react';
+import {View, Text, FlatList} from 'react-native';
+import axios from 'axios';
+import set from '@babel/runtime/helpers/esm/set';
 
 export default function({navigation}) {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://node-app-4fun.herokuapp.com/users/findAll')
+            .then( res =>{
+                setData(res.data)
+        })
+            .catch(err =>{
+                console.log(err)
+            });
+    },[]);
+
+     let renderItem = ({item, index})=>{
+        <View>
+            <Text>
+                {item.name}
+            </Text>
+        </View>
+    }
+
     return(
         <View>
-            <Text>Another details Screen</Text>
+            <FlatList
+                style={{borderWidth:1, height:'90%',width:'90%'}}
+                data={data}
+                renderItem={({item})=>(
+                    <View>
+                        <Text>{item.name}</Text>
+                    </View>
+                )}
+            />
         </View>
     );
+
 }
