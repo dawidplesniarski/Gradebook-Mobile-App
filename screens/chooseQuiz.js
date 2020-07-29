f1c52ceb04a8900a6e49225b441af6ec428e9449
dawidplesniarski <dawidplesniarski@gmail.com>
2020-07-29 22:11:45 +0200

    Check if user has permission to test or not

--- screens/chooseQuiz.js
+++ screens/chooseQuiz.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Picker} from 'react-native';
+import {View, Text, StyleSheet, Picker} from 'react-native';
+import Button from '../components/Button';
 import axios from 'axios';
+import {connect} from 'react-redux';
 
 
-
-const ChooseQuiz = ({navigation}) => {
+const ChooseQuiz = ({loginReducer, navigation}) => {
     const [data, setData] = useState([]);
     const [selectedItem, setSelectedItem] = useState('');
+    const [includesAlbum, setIncludesAlbum] = useState('false');
 
 
     async function getCategories(){
@@ -19,30 +21,60 @@ const ChooseQuiz = ({navigation}) => {
             });
     }
 
+    const checkPermission = async () => {
+        const {data} = await axios.get(`https://node-app-4fun.herokuapp.com/permission/findByCategory/${selectedItem}`);
+        if(data[0].userAlbums.includes(loginReducer.loginData.albumNo)){
+            setIncludesAlbum('true')
+        }else{
+            setIncludesAlbum('false');
+        }
+    }
+
     useEffect(() => {
         getCategories();
     },[]);
 
-    if(data.length ===0){
+    if(data.length === 0){
         return(
             <View/>
         )}
     else{
         return (
             <View style={styles.container}>
-
+                <Text style={styles.titleText}>Wybierz temat testu:</Text>
                 <Picker
+                    style={styles.datePicker}
                     selectedValue={selectedItem}
                     onValueChange={value => {setSelectedItem(value)}}>
                     {data.map((value, index)=><Picker.Item label={`${value}`} value={`${value}`} key={index}/>)}
                 </Picker>
+                <Button text={'Start test!'} isButtonDark={true} onPress={() => checkPermission()}/>
+                <Text>{includesAlbum}</Text>
             </View>
         );
     }
 }
 
+
+
 const styles = StyleSheet.create({
+    titleText:{
+        fontSize:20,
+        fontFamily: 'Futura',
+        textAlign: 'center',
+        paddingTop:10,
+    },
+    datePicker:{
+        margin:10,
+    },
+    container:{
+        display:'flex',
+    }
 
-})
+});
 
-export default ChooseQuiz;
+const mapStateToProps = ({ loginReducer}) => {
+    return { loginReducer };
+};
+
+export default connect(mapStateToProps)(ChooseQuiz);
